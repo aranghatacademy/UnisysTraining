276ef7b07ff209f7934b04032dbae7e01444359b
aranghatacademy <aranghatacademy@outlook.com>
2025-05-14 16:57:36 +0530

    Added Claims

---
+++ Autharization/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using AuthBAsics;
+
+namespace Autharization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var isValidUser = new PasswordVaultCredentialManager().Login("admin", "pass1234$$");
+
+            /*
+                admin
+                    -> CreateUser
+                    -> ChangePassword
+                    -> ChangePaymentGateway
+             */
+
+           if(isValidUser)
+            {
+               
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Role, "admin"),
+                    new Claim("Permission","CreateUser"),
+                    new Claim("Permission","ChangePassword"),
+                    new Claim("Permission","ChangePaymentGateway")
+                };
+
+                var identity = new ClaimsIdentity(new GenericIdentity("Admin"),claims);
+                var principal = new ClaimsPrincipal(identity);
+                
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/PaymentGateWay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PaymentGateWay
+    {
+        [PrincipalPermission(SecurityAction.Deny,Role = "Admin")]
+        public void ChangeGatewayApi(string apiKey)
+        {
+            
+        }
+    }
+}
