90c44b75bd8e74de4fe2397e8a10e376dbe1001e
aranghatacademy <aranghatacademy@outlook.com>
2025-05-15 14:38:26 +0530

    Added Jwt Token Validator

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,4 +1,8 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ToDoApi.Services;
 
 namespace ToDoApi
@@ -14,9 +18,48 @@ namespace ToDoApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
 
-            builder.Services.AddAuthentication();
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                            .AddJwtBearer(options =>
+                            {
+                                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                                {
+                                    ValidateAudience = false,
+                                    ValidateLifetime = true,
+                                    ValidateIssuer = false,
+                                    ValidateIssuerSigningKey = true,
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                                                            .UTF8
+                                                            .GetBytes("lhds4545nklnk6ljlk45654kj6l54j654nm,n,mn4b54564565nm,n")),
+                                };
+                            });
+
             builder.Services.AddAuthorization();
 
 
